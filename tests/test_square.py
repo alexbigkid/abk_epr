ad73b06b1add39abd99bf46da427406a4a75ac56
Alex Berger <alexbigkid@users.noreply.github.com>
2021-03-18 16:16:24 -0700

    e simple working pytest project

---
+++ tests/test_square.py
@@ -0,0 +1,16 @@
+import math
+
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def testsquare():
+   num = 7
+   assert 7*7 == 49
+
+
+def tesequality():
+   assert 10 == 11
+
+def test_myfunction(tmp_path):
+    pass
